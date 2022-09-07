540dd2938f04819fc28161d2b1eb6880223a0fe8
Lebedev Timofey <lebedev.timofey007@gmail.com>
2022-09-07 17:28:03 +0300

    Lesson4

---
+++ Creator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Building
+{
+    internal class Creator
+    {
+        private static Dictionary<long, Building> _created = new Dictionary<long, Building>();
+        private static long currentId = 0L;
+
+        public static Building create(string type)
+        {
+            Building newBuilding = Parse(type);
+            _created.Add(currentId++, newBuilding);
+            return newBuilding;
+            
+        }
+
+        public static void Remove(long id)
+        {
+            _created.Remove(id);
+        }
+
+        private static Building Parse(string type)
+        {
+            switch (type)
+            {
+                case ("struct"):
+                    return new StructBuilding();
+                default:
+                    throw new ArgumentException("Invalid argument");
+            }
+        }
+    }
+}
